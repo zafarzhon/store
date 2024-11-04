fef00ff5b436bf0e3ed71d8044c4f5f539576113
zafarzhon <odilovzafarzhon@gmail.com>
2024-11-05 02:02:05 +0300

    Create a bl_product_service module

---
+++ app/bl_product_service/src/main/java/com/primestore/bl_product_service/BlProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.primestore.bl_product_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlProductServiceApplication.class, args);
+    }
+
+}
---
+++ app/bl_product_service/src/test/java/com/primestore/bl_product_service/BlProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primestore.bl_product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
