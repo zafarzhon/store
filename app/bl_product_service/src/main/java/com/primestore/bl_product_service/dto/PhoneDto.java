007968169aaeaf2c7951c225c027280124eb4bb7
zafarzhon <odilovzafarzhon@gmail.com>
2024-11-09 17:50:47 +0300

    Update dtos

---
+++ app/bl_product_service/src/main/java/com/primestore/bl_product_service/dto/PhoneDto.java
@@ -0,0 +1,38 @@
+package com.primestore.bl_product_service.dto;
+
+import com.primestore.bl_product_service.domain.Battery;
+import com.primestore.bl_product_service.domain.Cpu;
+import com.primestore.bl_product_service.domain.Display;
+import com.primestore.bl_product_service.domain.Memory;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * @author zafarzhon
+ * @link <a href="https://github.com/zafarzhon">github</a>
+ */
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+public class PhoneDto extends ProductDto {
+    private Cpu cpu;
+    private Memory memory;
+    private Display display;
+    private Battery battery;
+    private String operationSystem;
+    private Boolean hasExMemoryCardSlot;
+    private Integer countSimCards;
+    private Boolean esimSupport;
+    private String communicationStandard;
+    private Integer countMainCamera;
+    private String mainCameraResolution;
+    private Boolean hasNFC;
+    private String bluetooth;
+    private String wifi;
+}
